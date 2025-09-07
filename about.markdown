5881e8a82a7aa4fa625c5476e299c38ba91cd1a3
Idavill <idamariavilladsen@gmail.com>
2025-09-07 16:43:44 +0200

    en masse ændringer

--- about.markdown
+++ about.markdown
@@ -6,4 +6,8 @@ permalink: /about/
 
 `Software developer with a playful design mindset <3`
 
+`This site showcases some of my most recent projects`
+
+`Software development | Interaction design | 3D modelling | Data processing`
+
 <!-- ![Alt text]({{ site.baseurl }}/assets/images/fun.png) -->
--- assets/sketch/sketch.js
+++ assets/sketch/sketch.js
@@ -1,6 +1,6 @@
-var num = 2222;
-var noiseScale = 100,
-  noiseStrength = 1;
+var num = 1222; // 2222
+var noiseScale = 80, // 100
+  noiseStrength = 1.5; // 1
 var particles = [num];
 var rectangle;
 let previewImageSize = 400;
@@ -19,6 +19,8 @@ let time = 0;
 let spherePosition = 0;
 let pg = null;
 let webglContexts = [];
+let op;
+let down;
 
 function sketch1(p) {
   let shape;
@@ -54,8 +56,8 @@ function preload() {
   img1 = loadImage("assets/images/mm.gif", handleImage, handleError);
   img2 = loadImage("assets/images/sentinel.gif", handleImage, handleError);
   img3 = loadImage("assets/images/ideadots.png", handleImage, handleError);
-  img4 = loadImage("assets/images/vr.png", handleImage, handleError);
-  img5 = loadImage("assets/images/weave1.jpg", handleImage, handleError);
+  img4 = loadImage("assets/images/fauna2.gif", handleImage, handleError);
+  img5 = loadImage("assets/images/flax2.png", handleImage, handleError);
   //shape = loadModel("assets/models/flat.obj");
 }
 function handleImage(img) {
@@ -67,6 +69,8 @@ function handleError(event) {
 
 function setup() {
   describe("purple sand particles mimicking water flow");
+
+  pg = createGraphics(previewImageSize, previewImageSize, WEBGL);
 
   imageMap.set("/jekyll/update/2025/09/06/urban", img2);
   imageMap.set("/jekyll/update/2025/09/06/meditations", img1);
@@ -91,8 +95,21 @@ function setup() {
 }
 
 function draw() {
-  time += 1;
+  if (time === 0) {
+    down = false;
+  } else if (time === 255) {
+    down = true;
+  }
+
+  if (down) {
+    time -= 1;
+  } else if (!down) {
+    time += 1;
+  }
+
+  console.log(time);
   fill(0, 10);
+
   for (let i = 0; i < particles.length; i++) {
     particles[i].run();
   }
@@ -116,24 +133,43 @@ function windowResized() {
 }
 
 function makeWobbleSphere() {
-  if (webglContexts.length >= 2) {
-    webglContexts[0] = null; // release oldest
-    webglContexts.shift();
-  }
-  pg = createGraphics(previewImageSize, previewImageSize, WEBGL);
-  webglContexts.push(pg);
+  pg.push();
 
   pg.background(255, 192, 203);
-  let c = color("orchid");
-  pg.ambientLight(c);
-  pg.directionalLight(255, 0, 0, 0, 1, 0);
+  //let c = color("orchid");
+  //pg.ambientLight(c);
+  pg.ambientLight(150 - time / 10, 50, 214 - time / 10);
+
+  pg.directionalLight(time, 50, 100, 1, time / 10, -time / 10);
   pg.noStroke();
 
-  let detail = 100; // Controls resolution
-  let radius = 100;
+  let detail = 20;
+  pg.rotateY(frameCount * 0.002); // Add rotation for visual interest
 
-  sphere1 = pg.sphere(100, detail, detail);
-  sphere1.translate(100, 10, 10);
+  //pg.box(100, 100);
+  pg.sphere(100, detail, detail);
+
+  pg.pop();
+
+  // if (webglContexts.length >= 2) {
+  //   webglContexts[0] = null; // release oldest
+  //   webglContexts.shift();
+  // }
+  //pg = createGraphics(previewImageSize, previewImageSize, WEBGL);
+  //webglContexts.push(pg);
+  // x = 0;
+
+  // pg.background(255, 192, 203);
+  // let c = color("orchid");
+  // pg.ambientLight(c);
+  // pg.directionalLight(255, 0, 0, x, 1, 0);
+  // pg.noStroke();
+
+  // let detail = 100; // Controls resolution
+  // let radius = 100;
+
+  // sphere1 = pg.sphere(100, detail, detail);
+  //sphere1.translate(100, 10, 10);
   image(
     pg,
     windowWidth - previewPosition,
@@ -194,7 +230,7 @@ class Particle {
     }
   }
   update() {
-    fill(50, 0, 255);
+    fill(50, 0, 255, 10);
     ellipse(this.loc.x, this.loc.y, this.loc.z);
   }
 }
