d38f7f592eee1daec41ca58cbd24e23fffa2a0d2
Idavill <idamariavilladsen@gmail.com>
2025-09-07 03:20:25 +0200

    another good baseline

--- assets/sketch/sketch.js
+++ assets/sketch/sketch.js
@@ -2,14 +2,26 @@ var num = 2222;
 var noiseScale = 100,
   noiseStrength = 1;
 var particles = [num];
+var rectangle;
+let img1;
+let img2;
+let img3;
+
+function preload() {
+  //img1 = loadImage("assets/images/fun.png");
+  //img2 = loadImage("../images/td.png");
+  //img3 = loadImage("../images/vr.png");
+}
 
 function setup() {
   describe("purple sand particles mimicking water flow");
 
+  //let postHeader = select("a.post-link");
+  //postHeader.mouseOver(drawRandomCircle);
+
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
-
   noStroke();
   for (let i = 0; i < num; i++) {
     var loc = createVector(random(width * 1.2), random(height), 2);
@@ -22,18 +34,36 @@ function setup() {
 }
 
 function draw() {
-  // background(0);
+  //rect(windowWidth - 150, windowHeight - 150, 100, 100);
   fill(0, 10);
-  //rect(255, 192, 203);
-  //rect(255, 192, width, height);
   for (let i = 0; i < particles.length; i++) {
     particles[i].run();
   }
 }
 
+// function write(textvar) {
+//   textSize(20);
+//   textAlign(CENTER);
+
+//   //Add font
+//   //textFont("Courier New");
+//   fill(255, 0, 0);
+//   text(textvar);
+// }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+// drawRandomCircle = function (param) {
+//   ellipse(random(width), random(height), 25, 25);
+//   console.log("halløj");
+// };
+
+// function mouseMoved() {
+//   drawRandomCircle();
+//    let cnv = select('canvas');
+// }
 
 class Particle {
   constructor(_loc, _dir, _speed) {
