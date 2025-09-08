a0635cf44f9177dfb18aa8cb0371d4f9afe5aeac
Idavill <idamariavilladsen@gmail.com>
2025-09-08 23:23:59 +0200

    fun sphere instead

--- assets/sketch/sketch.js
+++ assets/sketch/sketch.js
@@ -4,6 +4,7 @@ let canvas2;
 let pageHeight;
 let heightOffset = 170;
 let canvasSize = 300;
+let particles;
 
 function s1(p) {
   p.setup = function () {
@@ -17,14 +18,15 @@ function s1(p) {
     let h = p.windowHeight / halfh;
     canvas1.position(p.windowWidth / 2 + 100, h + heightOffset);
 
-    model = p.loadModel("/me/assets/models/by.obj", { normalize: true });
+    //model = p.loadModel("/me/assets/models/by.obj", { normalize: true });
   };
   p.draw = function () {
     p.background(255, 192, 203);
-    p.lights(255, 192, 203);
-    //p.box(100);
-    p.rotateY(p.frameCount * 0.01);
-    p.box(100);
+    let c = p.color(255, 150, 203);
+    p.ambientLight(c);
+    p.orbitControl();
+    p.rotateY(p.frameCount * 0.001);
+    p.sphere(120, 20, 10);
   };
   p.windowResized = function () {
     let halfh = p.windowHeight / 2;
@@ -60,14 +62,6 @@ function s2(p) {
 
 drawRandomCircle = function (titleid) {
   console.log(titleid);
-  // let path = imageMap.get(titleid);
-  // if (titleid === "/jekyll/update/2025/09/06/ideadots") {
-  //   currentGraphics = path;
-  //   currentImage = null;
-  // } else {
-  //   currentImage = path;
-  //   currentGraphics = null;
-  // }
 };
 
 new p5(s2);
