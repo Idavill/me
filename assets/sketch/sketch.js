e3ec636b9cfacd1208637419c5ec22edffe7c95f
Idavill <idamariavilladsen@gmail.com>
2025-09-08 22:08:49 +0200

    cute baseline

--- assets/sketch/sketch.js
+++ assets/sketch/sketch.js
@@ -1,17 +1,20 @@
+let model;
+
 function s1(p) {
   p.setup = function () {
-    p.createCanvas(720, 200, p.WEBGL).parent("pfive-container");
-    p.background(0);
+    p.createCanvas(200, 200, p.WEBGL).parent("pfive-container");
+    p.background(255, 192, 203);
   };
   p.draw = function () {
-    p.circle(p.mouseX, p.mouseY, 50);
+    p.orbitControl();
+    p.box();
   };
 }
 
 function s2(p) {
   p.setup = function () {
-    p.createCanvas(720, 200).parent("pfive-container");
-    p.background(255);
+    p.createCanvas(200, 200).parent("pfive-container");
+    p.background(255, 192, 203);
     p.fill(0);
     p.stroke(255);
   };
